f7ad0a2a89ef23192d089fc6d08a363ca6cb0580
popodelianlian123 <popodelianlian123@gmail.com>
2026-09-24 04:11:31 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+
+const CACHE='family-menu-v1';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET') return;
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
